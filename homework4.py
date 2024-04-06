19ac3b1748dec7a8613df63d57708e00eb0f63bd
Anton-Antonov-udm <54397005+Anton-Antonov-udm@users.noreply.github.com>
2024-04-07 00:46:28 +0400

    Add files via upload
    Загрузка файла ДЗ4

---
+++ homework4.py
@@ -0,0 +1,7 @@
+immutable_var = 'winter', 'summer', 'spring', 'autumn' #Создайте переменную immutable_var и присвойте ей кортеж из нескольких элементов разных типов данных.
+print(immutable_var)              #Выполните операции вывода кортежа immutable_var на экран.
+#Попытайтесь изменить элементы кортежа immutable_var. Объясните, почему нельзя изменить значения элементов кортежа.
+#immutable_var[1] = 'april' - невозможно изменить значение, является элементом кортежа
+mutable_list = [34, 'Anton', True] #Создайте переменную mutable_list и присвойте ей список из нескольких элементов.
+mutable_list[2] = False #Измените элементы списка mutable_list.
+print(mutable_list) #Выведите на экран измененный список mutable_list.
